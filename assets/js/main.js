4ef37d8a2e3a3e29f7e6e287c48995f6727aea60
Jumzeey <umatonjayrex01@gmail.com>
2021-07-10 21:01:44 +0100

    mobile menu complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,38 @@
+/*===================== GLOBAL FUNCTIONS ===================*/
+function _(x){
+    return document.getElementById(x);
+}
+function __(x){
+    return document.querySelectorAll(x)
+}
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = _('nav-menu'),
+    navToggle = _('nav-toggle'),
+    navClose = _('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink =__('.nav__link');
 
+function linkAction(){
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
